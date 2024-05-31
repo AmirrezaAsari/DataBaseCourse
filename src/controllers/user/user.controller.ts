994123d2b1623a40ba5348e53d68955d65c5c7d6
unknown <amirreza.asari2@gmail.com>
2024-05-31 19:36:21 +0330

    feat: update user profile

--- src/controllers/user/user.controller.ts
+++ src/controllers/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request, Put, Body } from '@nestjs/common';
 import { UserService } from '../../modules/user/services/user/user.service';
 import { UserGuard } from '../../guards/user.guard';
+import { UpdateUserDto } from '../../requests/user/update-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -8,7 +9,7 @@ export class UserController {
 
   @UseGuards(UserGuard)
   @Get('profile')
-  async show(@Request() req: any): Promise<any> {
+  async show(@Request() req): Promise<any> {
     const user = await this.userService.findOne(req.user.sub);
     const result = {
       name: user.name,
@@ -18,6 +19,23 @@ export class UserController {
     };
     return {
       message: 'user.profile.show.success',
+      data: result,
+    };
+  }
+
+  @UseGuards(UserGuard)
+  @Put('profile')
+  async update(@Request() req, @Body() body: UpdateUserDto): Promise<any> {
+    const user = await this.userService.findOne(req.user.sub);
+    const inputs = {
+      name: body.name || user.name,
+      email: body.email || user.email,
+      password: body.password || user.password,
+      type: user.type,
+    };
+    const result = await this.userService.update(user.id, inputs);
+    return {
+      message: 'user.profile.update.success',
       data: result,
     };
   }
